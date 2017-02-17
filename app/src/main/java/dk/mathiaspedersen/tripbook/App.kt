b936bdb3fe1d5cc5d74f2649773420c783d7ea2f
Mathias Pedersen <kontakt@mathiaspedersen.dk>
2017-02-17 18:57:25 +0100

    Leak detection and Firebase disk persistence

--- app/src/main/java/dk/mathiaspedersen/tripbook/App.kt
+++ app/src/main/java/dk/mathiaspedersen/tripbook/App.kt
@@ -1,6 +1,8 @@
 package dk.mathiaspedersen.tripbook
 
 import android.app.Application
+import com.google.firebase.database.FirebaseDatabase
+import com.squareup.leakcanary.LeakCanary
 import dk.mathiaspedersen.tripbook.presentation.injection.ApplicationComponent
 import dk.mathiaspedersen.tripbook.presentation.injection.ApplicationModule
 import dk.mathiaspedersen.tripbook.presentation.injection.DaggerApplicationComponent
@@ -14,11 +16,22 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeDagger()
+        initializeLeakDetection()
+        enableDiskPersistence()
     }
 
     fun initializeDagger() {
         graph = DaggerApplicationComponent.builder()
                 .applicationModule(ApplicationModule(this))
                 .build()
+    }
+
+    fun initializeLeakDetection() {
+        if (LeakCanary.isInAnalyzerProcess(this)) return
+        if (BuildConfig.DEBUG) LeakCanary.install(this)
+    }
+
+    fun enableDiskPersistence() {
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
     }
 }
