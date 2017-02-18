b40b0ffad6f00cb88831936ae8219a125cac6256
Mathias Pedersen <kontakt@mathiaspedersen.dk>
2017-02-18 20:28:08 +0100

    Navigation menu implemented

--- app/src/main/java/dk/mathiaspedersen/tripbook/presentation/activity/MainActivity.kt
+++ app/src/main/java/dk/mathiaspedersen/tripbook/presentation/activity/MainActivity.kt
@@ -2,7 +2,11 @@ package dk.mathiaspedersen.tripbook.presentation.activity
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -16,7 +20,7 @@ import dk.mathiaspedersen.tripbook.presentation.view.MainView
 import org.jetbrains.anko.find
 import javax.inject.Inject
 
-class MainActivity : BaseActivity(), MainView {
+class MainActivity : BaseActivity(), MainView, NavigationView.OnNavigationItemSelectedListener {
 
     override val layoutResource = R.layout.activity_main
 
@@ -34,6 +38,15 @@ class MainActivity : BaseActivity(), MainView {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun injectDependencies(applicationComponent: ApplicationComponent) {
@@ -59,9 +72,18 @@ class MainActivity : BaseActivity(), MainView {
         presenter.onPause()
     }
 
+    override fun onBackPressed() {
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
@@ -77,5 +99,28 @@ class MainActivity : BaseActivity(), MainView {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
