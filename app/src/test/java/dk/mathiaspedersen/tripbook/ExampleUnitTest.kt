c9a35228d3ab23873400f2671c41c56be3ffd375
Mathias Pedersen <kontakt@mathiaspedersen.dk>
2017-06-01 13:11:12 +0200

    Removed Spek and Expekt in favor of jUnit

---
+++ app/src/test/java/dk/mathiaspedersen/tripbook/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package dk.mathiaspedersen.tripbook
+
+import org.junit.Before
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun name() {
+
+    }
+}
